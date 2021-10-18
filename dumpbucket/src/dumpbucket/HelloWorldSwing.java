e34ebb99e4bf4c25056450238729a36c4e4a308b
224717 <224717@424-N211-143.mcpsmd.org>
2021-10-18 10:52:12 -0400

    HelloWorldSwing now shows 2 images of space

--- dumpbucket/src/dumpbucket/HelloWorldSwing.java
+++ dumpbucket/src/dumpbucket/HelloWorldSwing.java
@@ -1,32 +1,42 @@
 package dumpbucket;
+import java.awt.GridLayout;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.net.URL;
+
+import javax.imageio.ImageIO;
 import javax.swing.*; 
 
 public class HelloWorldSwing {
-    /**
-     * Create the GUI and show it.  For thread safety,
-     * this method should be invoked from the
-     * event-dispatching thread.
-     */
-    private static void createAndShowGUI() {
-        //Create and set up the window.
-        JFrame frame = new JFrame("HelloWorldSwing");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        //Add the ubiquitous "Hello World" label.
-        JLabel label = new JLabel("Hello World");
+    private static void createAndShowGUI() throws Exception{
+        JFrame frame = new JFrame("HelloWorldSwing");
+        frame.setLayout(new GridLayout(1, 2));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        BufferedImage myPicture = ImageIO.read(new URL("https://cdn.mos.cms.futurecdn.net/jfkAU4tM8XMUAPZDm4h5Nh-1200-80.jpeg"));
+        BufferedImage myPicture2 = ImageIO.read(new URL("https://i.insider.com/5dfab6ce855cc20c514e79f6?width=1000&format=jpeg&auto=webp"));
+        JLabel label = new JLabel(new ImageIcon(myPicture));
+        JLabel label2 = new JLabel(new ImageIcon(myPicture2));
         frame.getContentPane().add(label);
+        frame.getContentPane().add(label2);
 
         //Display the window.
         frame.pack();
         frame.setVisible(true);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
         //Schedule a job for the event-dispatching thread:
         //creating and showing this application's GUI.
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-                createAndShowGUI();
+                try {
+					createAndShowGUI();
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
             }
         });
     }
